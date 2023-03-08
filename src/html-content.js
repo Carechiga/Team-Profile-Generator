54fe27368522562270824f3a4e86c74990a48444
Carechiga <Carechiga43@gmail.com>
2023-03-08 07:35:28 -0800

    styling link needed to be adjusted after changing file structure

--- src/html-content.js
+++ src/html-content.js
@@ -7,7 +7,7 @@ const GenerateHtmlContent = (projectName, managerInfoHtml, engineerInfoHtml, int
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${projectName}</title>
-        <link rel="stylesheet" href="./dist/css/styles.css">
+        <link rel="stylesheet" href="./css/styles.css">
     </head>
     <body>
         <header>
